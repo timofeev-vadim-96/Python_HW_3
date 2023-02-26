65629311645ad78ef7ae62bdc1fe5468c078daaf
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-26 23:51:37 +0300

    task 18 добавил комментарии к коду

--- task_18.py
+++ task_18.py
@@ -20,12 +20,12 @@ temp_numb_1 = 0 # 1-е ближайшее число
 temp_numb_2 = 0 # 2-е ближайшее число
 flag = False # ближайшее число к find_N лишь одно
 for i in range(0, len(list_1)):
-    dif = find_N - list_1[i]
+    dif = find_N - list_1[i] #разница между числами
     if dif == 0: print(f'Введенное число {find_N} присутствует в списке')
-    elif min_dif > abs(dif): 
+    elif min_dif > abs(dif): # чем меньше разница между числами, тем ближе число по значению
         min_dif = abs(dif)
         temp_numb_1 = list_1[i]
-    elif min_dif == abs(dif): 
+    elif min_dif == abs(dif): # если имеется еще одно значению, по модулю столь же отдаленное
         flag = True
         temp_numb_2 = list_1[i] 
 if flag: print(f'Ближайшие к введенному числу - 2 числа: {temp_numb_1} и {temp_numb_2}')
